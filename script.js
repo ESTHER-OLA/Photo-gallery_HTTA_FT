7b27ab0ce85b5b7382b0013be4877052d9932b49
olaesther <estherolowo2018@gmail.com>
2023-11-21 16:53:43 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,48 @@
+const change = document.getElementById("image1");
+
+change.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "white";
+});
+
+const change2 = document.getElementById("image2");
+change2.addEventListener("click", (event) => {
+    event.target.style.background = "yellow";
+    event.target.style.color = "blue";
+});
+
+const change3 = document.getElementById("image3");
+change3.addEventListener("click", (event) => {
+    event.target.style.background = "gold";
+    event.target.style.color = "red";
+});
+
+const change4 = document.getElementById("image4");
+change4.addEventListener("click", (event) => {
+    event.target.style.background = "chartreuse";
+    event.target.style.color = "chocolate";
+});
+
+const change5 = document.getElementById("image5");
+change5.addEventListener("click", (event) => {
+    event.target.style.background = "purple";
+    event.target.style.color = "aqua";
+});
+
+const change6 = document.getElementById("image6");
+change6.addEventListener("click", (event) => {
+    event.target.style.background = "cyan";
+    event.target.style.color = "black";
+});
+
+const change7 = document.getElementById("image7");
+change7.addEventListener("click", (event) => {
+    event.target.style.background = "indigo";
+    event.target.style.color = "crimson";
+});
+
+const change8 = document.getElementById("image8");
+change8.addEventListener("click", (event) => {
+    event.target.style.background = "crimson";
+    event.target.style.color = "darkblue";
+});
